f691afae98b20238779998cbf993865226e57ca0
shivaramvennam1331 <shivaram.vennam@in.patrika.com>
2026-08-03 12:47:38 +0530

    refactor (version) : Chaged Some Versions to be able to work in iOS

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,8 @@
 module.exports = function (api) {
   api.cache(true);
   return {
+    // babel-preset-expo already injects react-native-worklets/plugin when
+    // reanimated is installed; listing it again applies the transform twice.
     presets: ['babel-preset-expo'],
-    plugins: [
-      'react-native-reanimated/plugin',
-    ],
   };
 };
